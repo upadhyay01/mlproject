e1cd198cc17765b940b3696367a31bbb669575ef
MRITYUNJAY UPADHYAY <upadhyaymrityunjay@gmail.com>
2023-03-15 19:23:40 +0530

    added file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+@dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
     test_data_path: str=os.path.join("artifacts","test.csv")
@@ -23,7 +24,7 @@ class DataIngestion:
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.train_data_path)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
